eb4086d55700b18951ab40800b7c6f0df9fd9ece
khink <email@example.com>
2011-04-08 13:31:18 +0000

    don't use PTSTranslationDomain anymore, so this will work on Plone 4 (tested on 4.0.5-pending)

--- Products/CurrencyUtility/__init__.py
+++ Products/CurrencyUtility/__init__.py
@@ -1,4 +1,4 @@
 from zope.i18nmessageid import MessageFactory
 CurrencyMessageFactory = MessageFactory('CurrencyUtility')
-from Products.PlacelessTranslationService.utility import PTSTranslationDomain
-currencydomain = PTSTranslationDomain('CurrencyUtility')
+#from Products.PlacelessTranslationService.utility import PTSTranslationDomain
+#currencydomain = PTSTranslationDomain('CurrencyUtility')
