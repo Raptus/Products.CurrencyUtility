47f0af676310de43567aa6566ad7f1c2406273cc
huub_bouma <email@example.com>
2011-10-13 13:27:32 +0000

    rounding no longer a fixed value of 5 cents

--- Products/CurrencyUtility/currency.py
+++ Products/CurrencyUtility/currency.py
@@ -13,13 +13,17 @@ class CurrencyAware(object):
     def __str__(self):
         return self.toString()
 
-    def __init__(self, value, currency=None):
+    def __init__(self, value, currency=None, rounding=None):
         """value is a price and IT's currency"""
         self.utility = getUtility(ICurrencyUtility)
         if currency is not None:
             value = float(value)/self.utility.getCurrencyFactor(currency)
         self.value = value
-        
+        if rounding is not None:
+            self.rounding = rounding
+        else:
+            rounding = 0.05 # default value, round to 5 cents
+
     def getCurrencySymbol(self):
         """returns the currency symbol"""
         return self.utility.getActiveCurrency()
@@ -29,20 +33,20 @@ class CurrencyAware(object):
         factor = self.utility.getCurrencyFactor(currency)
         return float(self.value) * factor
 
-    def getRoundedValue(self, currency=None, rounding=0.05):
+    def getRoundedValue(self, currency=None):
         """returns the value in the appropriate currency rounded to 0.05"""
         value = self.getValue(currency)
-        factor = 1.0 / rounding
+        factor = 1.0 / self.rounding
         return float(int(math.ceil(value*factor)))/factor
 
     def toString(self, currency=None):
         """returns the value in the appropriate currency rounded to 0.05 including the symbol"""
         return "%s %0.2f" % (self.utility.getCurrencySymbol(currency), self.getRoundedValue(currency))
-    
+
     def valueToString(self, currency=None):
         """returns the value in the appropriate currency rounded to 0.05"""
         return "%0.2f" % self.getRoundedValue(currency)
-    
+
     def safeToString(self, currency=None):
         """returns the value in the appropriate currency rounded to 0.05 including the currency-short-name"""
         return "%s %0.2f" % (self.utility.getActiveCurrency(currency), self.getRoundedValue(currency))
